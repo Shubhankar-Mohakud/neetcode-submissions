3d5b9f4c2d222cfe75fc3e5c058e522c88c51e33
Shubhankar Mohakud <157949746+Shubhankar-Mohakud@users.noreply.github.com>
2026-05-24 14:41:23 +0530

    Add: remove-linked-list-elements - submission-0

---
+++ Data Structures & Algorithms/remove-linked-list-elements/submission-0.cpp
@@ -0,0 +1,34 @@
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode() : val(0), next(nullptr) {}
+ *     ListNode(int x) : val(x), next(nullptr) {}
+ *     ListNode(int x, ListNode *next) : val(x), next(next) {}
+ * };
+ */
+using ln = ListNode;
+class Solution {
+public:
+    ListNode* removeElements(ListNode* head, int val) {
+        ln* curr = head;
+        ln* prev = NULL;
+        while(curr){
+            if(curr->val==val){
+                if(curr==head){
+                    head=head->next;
+                    curr=curr->next;
+                }
+                else {
+                    prev->next = curr->next;
+                    curr=curr->next;
+                }
+            } else {
+                prev=curr;
+                curr=curr->next;
+            }
+        }
+        return head;
+    }
+};
